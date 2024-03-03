0e79dcc73e4a805d91e5f2a5d84ffaf3caee7366
digitpdigitm <dpraktikastudio@gmail.com>
2024-03-03 18:38:41 +0700

    fix: modularize middleware bypass and added course-review on bypass

--- src/modules/auth/decoding.js
+++ src/modules/auth/decoding.js
@@ -3,9 +3,33 @@ const jwt = require('jsonwebtoken');
 const secretKey = 'thisIsOurSecret'; // Replace with your actual secret key
 const Users = require('../../models/users');
 
+const determineBypass = (currentPath, method) => {
+  let isBypass = false
+
+  if (method !== 'GET') return false
+
+  // /courses
+  if (currentPath.length == 2 && currentPath[1].includes('courses')) {
+    isBypass = true
+  }
+
+  // /courses/detail
+  if (currentPath.length == 3 && currentPath[1].includes('courses') && currentPath[2].includes('detail')) {
+    isBypass = true
+  }
+
+  // /courses/review
+  if (currentPath.length == 3 && currentPath[1].includes('courses') && currentPath[2].includes('review')) {
+    isBypass = true
+  }
+
+  return isBypass
+}
+
 const decodeSessionTokenMiddleware = async (req, res, next) => {
   let currentPath = req.originalUrl.split("/")
-
+  const method = req.method
+  const isBypass = determineBypass(currentPath, method)
   const sessionToken = req.headers.authorization && req.headers.authorization.split(' ')[1];
 
   if (sessionToken) {
@@ -13,24 +37,19 @@ const decodeSessionTokenMiddleware = async (req, res, next) => {
       const decoded = jwt.verify(sessionToken, secretKey);
       req.user = decoded;
 
-      const user = await Users.findOne({ email:req.user.email }).select('-password').exec();
+      const user = await Users.findOne({ email: req.user.email }).select('-password').exec();
       res.locals.user = user
-      
+
       next();
     } catch (error) {
-      if (currentPath.length == 3 && currentPath[1] == 'courses') {
-        next();
-      }
+      if (isBypass) return next()
       // Handle token verification error
       console.error(error);
       return res.status(401).json({ message: 'Invalid session token' });
     }
   } else {
-    if (currentPath.length == 3 && currentPath[1] == 'courses') {
-      next();
-    } else {
-      return res.status(401).json({ message: 'Session token not provided' });
-    }
+    if (isBypass) return next()
+    return res.status(401).json({ message: 'Session token not provided' });
   }
 };
 
